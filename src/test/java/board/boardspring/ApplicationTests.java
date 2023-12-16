2c4b80edd3561b52d33de87ab3d547a43e6fbce9
Kang Sanghyun <kangsang3073@gmail.com>
2023-12-16 20:17:15 +0900

    first commit

---
+++ src/test/java/board/boardspring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package board.boardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
